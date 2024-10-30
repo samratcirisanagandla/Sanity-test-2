f7a79d51a60669720ce5716ff0a1d90ad8a1c405
samratcirisanagandla <cirisanagandla.s@mobiusdtaas.ai>
2024-10-30 17:53:17 +0530

    Add dummy tests

---
+++ test-dummy.py
@@ -0,0 +1,14 @@
+# test_dummy.py
+import pytest
+
+def test_addition():
+    assert 1 + 1 == 2
+
+def test_subtraction():
+    assert 5 - 3 == 2
+
+def test_multiplication():
+    assert 3 * 4 == 12
+
+def test_division():
+    assert 10 / 2 == 5
